935677f13f58ac63f6ce2a946150bdb8e275d9fb
mohye <mohye20@gmail.com>
2024-11-30 12:24:04 +0200

    API Response

---
+++ MagicVilla_VillaAPI/Models/APIRespoinse.cs
@@ -0,0 +1,16 @@
+﻿using System.Net;
+
+namespace MagicVilla_VillaAPI.Models
+{
+	public class APIRespoinse
+	{
+
+		public HttpStatusCode StatusCode { get; set; }
+
+		public bool IsSuccess { get; set; }
+
+		public List<string> ErrorMessage { get; set; }
+
+		public object Result { get; set; }
+	}
+}
