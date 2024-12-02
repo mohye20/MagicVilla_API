3f331a2d0a74abe3501e470945ae5fd63f8d83c1
mohye <mohye20@gmail.com>
2024-12-02 11:25:48 +0200

    Base Service for API

---
+++ MagicVilla_Web/Services/BaseService.cs
@@ -0,0 +1,70 @@
+using System.Text;
+using System.Text.Json.Serialization;
+using MagicVilla_Utility;
+using MagicVilla_Web.Models;
+using MagicVilla_Web.Services.IServices;
+using Newtonsoft.Json;
+
+namespace MagicVilla_Web.Services;
+
+public class BaseService : IBaseService
+{
+    public APIResponse responseModel { get; set; }
+
+    public IHttpClientFactory HttpClientFactory { get; set; }
+
+    public BaseService(IHttpClientFactory httpClientFactory)
+    {
+        HttpClientFactory = httpClientFactory;
+        this.responseModel = new();
+    }
+
+    public async Task<T> SendAsync<T>(APIRequest request)
+    {
+        try
+        {
+            var client = HttpClientFactory.CreateClient("MagicAPI");
+            HttpRequestMessage message = new();
+            message.Headers.Add("Accept", "application/json");
+            message.RequestUri = new Uri(request.Url);
+            if (request.Data is not null)
+            {
+                message.Content = new StringContent(JsonConvert.SerializeObject(request.Data),
+                    Encoding.UTF8, "application/json");
+            }
+
+            switch (request.ApiType)
+            {
+                case SD.ApiType.POST:
+                    message.Method = HttpMethod.Post;
+                    break;
+                case SD.ApiType.PUT:
+                    message.Method = HttpMethod.Put;
+                    break;
+                case SD.ApiType.DELETE:
+                    message.Method = HttpMethod.Delete;
+                    break;
+                default:
+                    message.Method = HttpMethod.Get;
+                    break;
+            }
+
+            HttpResponseMessage apiResponse = null;
+            apiResponse = await client.SendAsync(message);
+            var apiContent = await apiResponse.Content.ReadAsStringAsync();
+            var ApiResponse = JsonConvert.DeserializeObject<T>(apiContent);
+            return ApiResponse;
+        }
+        catch (Exception e)
+        {
+            var dto = new APIResponse()
+            {
+                ErrorMessage = new List<string> { Convert.ToString(e.Message) },
+                IsSuccess = false,
+            };
+            var res = JsonConvert.SerializeObject(dto);
+            var APIResponse = JsonConvert.DeserializeObject<T>(res);
+            return APIResponse;
+        }
+    }
+}
