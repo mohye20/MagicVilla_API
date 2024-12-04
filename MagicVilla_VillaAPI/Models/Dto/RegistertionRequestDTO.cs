e22377ae1bed418f1e770671d54edc4e04242911
mohye <mohye20@gmail.com>
2024-12-04 03:44:03 +0200

    Add Models for Login and Registeration

---
+++ MagicVilla_VillaAPI/Models/Dto/RegistertionRequestDTO.cs
@@ -0,0 +1,12 @@
+namespace MagicVilla_VillaAPI.Models.Dto;
+
+public class RegistertionRequestDTO
+{
+    public string UserName { get; set; }
+    
+    public string Name { get; set; }
+    
+    public string Password { get; set; }
+    
+    public string Role { get; set; }
+}
